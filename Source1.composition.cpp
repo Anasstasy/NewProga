2cd17d410a7e5547980fcb3625fd484a4f7a2800
Anasstasy <118056682+Anasstasy@users.noreply.github.com>
2023-03-10 09:41:31 +0200

    Update Source1.composition.cpp

--- Source1.composition.cpp
+++ Source1.composition.cpp
@@ -7,73 +7,45 @@ using namespace std;
 
 //1) написать пример кода на композицию - должно быть два класса, один из которых - ЦЕЛОЕ, а второй - неотъемлемая часть целого.в мейне написать проверочные тесты.
 
-class Dog 
-{
-public:
-    void setBreed(const string& breed) 
-    {
-        m_breed = breed;
-    }
-    const string& getBreed() const 
-    {
-        return m_breed;
-    }
-    void setAge(int age) 
-    {
-        m_age = age;
-    }
-    int getAge() const 
-    {
-        return m_age;
-    }
-};
-
 class Animal {
-public:
-    void setSpecies(const string& species) 
-    {
-        m_species = species;
-    }
-    const string& getSpecies() const 
-    {
-        return m_species;
-    }
-    void setDog(const Dog& dog) 
-    {
-        m_dog = dog;
-    }
-    const Dog& getDog() const 
-    {
-        return m_dog;
-    }
 private:
-    string m_species;
-    Dog m_dog;
+    string species_;
+public:
+    Animal(const string& species) : species_(species) {}
+    string getSpecies() {
+        return species_;
+    }
 };
 
-int main() 
-{
-    Dog dog;
-    dog.setBreed("Schipperke");
-    dog.setAge(5);
+class Dog {
+private:
+    string name_;
+    Animal* animal_;
+public:
+    Dog(const string& name, const string& species) :
+        name_(name), animal_(new Animal(species)) {}
 
-    Animal animal;
-    animal.setSpecies("Mammal");
-    animal.setDog(dog);
+    ~Dog() {
+        delete animal_;
+    }
 
-    cout << "Animal species: " << animal.getSpecies() << endl;
-    cout << "Dog breed: " << animal.getDog().getBreed() << endl;
-    cout << "Dog age: " << animal.getDog().getAge() << endl;
+    void bark() {
+        cout << name_ << " the " << animal_->getSpecies() << " is barking." << endl;
+    }
+};
 
+int main() {
+    Dog myDog("Zhuzha", "Schipperke");
+    myDog.bark();
     return 0;
 }
 
 //придумать по 3 примера на наследование и реализацию интерфейсов
 /*
    Наследование
-   Солнце - лучик
-   Фигуры - ромб
-   Овощи - баклажан
+   Фрукт - манго
+   Фигура - ромб
+   Овощь - баклажан
 
    Реализация интерфейсов
    Интерфейс "Музыкальный инструмент" - класс "Гитара" - переменные класса "Струнный щипковый музыкальный инструмент с корпусом в форме восьмёрки"
